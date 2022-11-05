87a0d71f273aa7aa8f6a2cad190328964dbb315e
lucas <lucas.linard@outlook.com>
2022-11-05 20:10:08 -0300

    corrigido questoes de seguranca em login

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,20 +1,26 @@
 const Router = require("express").Router();
 const MongoClient = require("mongodb").MongoClient;
-const login = Router
-
-
+const login = Router;
 
 login.get("/", async (req, res) => {
-    
-    const client = req.app.locals.bd
-    const collection = client.collection('Usuarios');
-    const dados = await collection.find({email: req.query.user, senha: req.query.senha}).toArray()
-        
-    if(dados.length == 1){    
-    res.json({ message: dados });
-    }    else {
+  const client = req.app.locals.bd;
+  const collection = client.collection("Usuarios");
+  const dados = await collection
+    .find({ email: req.query.user, senha: req.query.senha })
+    .toArray();
+
+  if (dados.length == 1) {
+    res.json({
+      message: {
+        id: dados[0]._id,
+        nome: dados[0].nome,
+        email: dados[0].email,
+        perfil: dados[0].perfil,
+      },
+    });
+  } else {
     res.json({ message: "Entrada inválida" });
-    }
-})
+  }
+});
 
 module.exports = login;
