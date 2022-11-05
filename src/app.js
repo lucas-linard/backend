efcc1b3fb80b243b4e5a2c0359a2f007288e0991
lucas <lucas.linard@outlook.com>
2022-11-05 19:25:13 -0300

    correcao do nome da rota de signup

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const logger = require('morgan')
 
 // import { uploadRouter } from './routes/upload'
 const uploadRouter = require('./routes/upload')
+const signgUp = require('./routes/signUp')
 // import { downloadRouter } from './routes/download'
 // import { newUser } from './routes/newUser'
 // import { novaDisciplina } from './routes/novaDisciplin
@@ -17,6 +18,7 @@ app.use(fileUpload())
 module.exports = app
 
  app.use('/upload', uploadRouter)
+    app.use('/signUp', signgUp)
 // app.use('/download', downloadRouter)
 // app.use('/newUser', newUser)
 // app.use('/novaDisciplina', novaDisciplina)
---
+++ src/routes/signUp.js
@@ -0,0 +1,63 @@
+const Router = require("express").Router();
+const MongoClient = require("mongodb").MongoClient;
+const singUp = Router
+
+function validaEntrada(query) {
+    if (
+      !!query.nome ||
+      !!query.email ||
+      !!query.senha ||
+      !!query.perfil
+    )
+      return true;
+    else return false;
+  }
+
+  function validaTipo(query) {
+    return query.perfil == "aluno" || query.perfil == "professor" ? true : false;
+  }
+
+  function validaEmail(query) {
+    let domain = (query.email. substring(query.email. lastIndexOf('@') +1))
+    return domain == "ftc.edu.br" ? true : false;
+  }
+
+  function validaTudo(query) {
+    return validaEntrada(query) && validaTipo(query) && validaEmail(query) ? true : false;
+  }
+ 
+
+  
+//TODO: VERIFICAR ANTES NO BANCO SE JA EXISTE O USUARIO
+// FAZER CONDICIONAL DE RESPOSTA
+// FAZER CONDICIONAL DE ERRO
+singUp.post("/", async (req, res) => {
+
+    
+    const client = req.app.locals.bd
+
+    const collection = client.collection('Usuarios');
+  
+    const dados = await collection.find({email: req.query.email}).toArray()
+
+            
+    if( validaTudo(req.query) && dados.length == 0){
+      try {
+        const insertResult = await collection.insertOne(req.query);
+        res.json({ message: 'OK' });
+      } catch (error) {
+        res.json({ message: error });
+      }
+    } else {
+      res.json({ message: "Entrada inválida" });
+    }
+});
+
+// { id
+//     NOMEM
+//     email
+//     senha
+//     tipo : prof ou aluno
+// }
+
+module.exports = singUp;
