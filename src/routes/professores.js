947f85b6ec6d30937774356458f4f24762200f9b
lucas <lucas.linard@outlook.com>
2022-11-21 19:27:55 -0300

    professor reversed

--- src/routes/professores.js
+++ src/routes/professores.js
@@ -7,9 +7,8 @@ professores.get('/', async (req, res) => {
 const client = req.app.locals.bd
 let collection = client.collection('Usuarios');
 try {
-    let dados = await collection.find({_id: new objectId(req.query.id)}).toArray()    
-    if(dados.length == 1){
-        collection = client.collection('Professores')
+    let dados = await collection.find({_id: new objectId(req.query.id)}).toArray()
+    if(dados.length == 1){        
         dados = await collection.find({}).toArray()
         const prof = dados.map((p) =>{ return {id: p._id.toString(), nome: p.nome, email: p.email}}) 
             res.status(200).send(prof)    
