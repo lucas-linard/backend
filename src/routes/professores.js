31f23e58e16624223ebb2bdce0ea6711b79fc144
lucas <lucas.linard@outlook.com>
2022-11-21 15:57:14 -0300

    maintance professor

--- src/routes/professores.js
+++ src/routes/professores.js
@@ -6,8 +6,10 @@ professores.get('/', async (req, res) => {
 
 const client = req.app.locals.bd
 const collection = client.collection('Usuarios');
+console.log(req.query.id)
 try {
     let dados = await collection.find({_id: new objectId(req.query.id)}).toArray()
+    console.log(dados)
     if(dados.length == 1){
         dados = await collection.find({perfil: 'professor'}).toArray()
         const prof = dados.map((p) =>{ return {id: p._id.toString(), nome: p.nome, email: p.email}}) 
